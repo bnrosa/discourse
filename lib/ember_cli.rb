cd646335b21a64e4e057cd4a9dd0459c697948e5
David Taylor <david@taylorhq.com>
2022-04-20 16:43:43 +0100

    Update lib/ember_cli.rb
    Co-authored-by: Alan Guo Xiang Tan <gxtan1990@gmail.com>

--- lib/ember_cli.rb
+++ lib/ember_cli.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EmberCli
-  ALIASES ||= {
+  ALIASES = {
     "application" => "discourse",
     "discourse/tests/test-support-rails" => "test-support",
     "discourse/tests/test-helpers-rails" => "test-helpers"
